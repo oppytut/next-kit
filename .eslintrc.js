ee52dc732750b3de441f9e0a2934534627857d9e
Arief Novianto <arief.novianto8@gmail.com>
2018-06-04 10:29:09 +0700

    change eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": ["airbnb-base", "react-app"],
+    "rules": {
+        "indent": ["error", "tab"],
+        "no-tabs": "off",
+        "keyword-spacing": [2, { "overrides": {
+            "if": { "after": false }
+        } }],
+        "no-console": ["error", {"allow": ["log"]}],
+        "comma-dangle": ["error", "never"],
+        "no-shadow": "off",
+        "class-methods-use-this": ["error", { "exceptMethods": ["render"] }],
+        "max-len": "off"
+    }
+};
