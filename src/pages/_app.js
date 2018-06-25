556c17c2ff2d52738d07799895d5e9331bde8303
Arief Novianto <arief.novianto8@gmail.com>
2018-06-25 16:06:42 +0700

    fixing something

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,17 +18,20 @@ const configureStore = (initialState, options) => createStore(reducer, initialSt
 class MyApp extends App {
 	static async getInitialProps({ Component, router, ctx }) {
 		let pageProps = {};
-		if(Component.getInitialProps) {
+
+		if (Component.getInitialProps) {
 			pageProps = await Component.getInitialProps(ctx);
 		}
+
 		return { pageProps };
 	}
 
 	render() {
 		const { Component, pageProps, store } = this.props;
+
 		return (
 			<Container>
-				<Provider store={ store }>
+				<Provider store={store}>
 					<Component {...pageProps} />
 				</Provider>
 			</Container>
