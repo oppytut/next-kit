4ad5dc26f36b23a77e2138f1d4bf0b33db56536e
Arief Novianto <arief.novianto8@gmail.com>
2018-06-25 16:07:23 +0700

    remove if keyword-spacing rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
     "rules": {
         "indent": ["error", "tab"],
         "no-tabs": "off",
-        "keyword-spacing": [2, { "overrides": {
-            "if": { "after": false }
-        } }],
         "no-console": ["error", {"allow": ["log"]}],
         "comma-dangle": ["error", "never"],
         "no-shadow": "off",
