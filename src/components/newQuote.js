c4deaeba40ae82749c1d5635d5cd78a73c73b96b
Arief Novianto <arief.novianto8@gmail.com>
2018-06-26 17:23:16 +0700

    change component to function

--- src/components/newQuote.js
+++ src/components/newQuote.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'; // must be in scope
+import React from 'react'; // must be in scope
 import { Card } from 'antd';
 import styled from 'styled-components';
 
-// import mzLogger from '../libs/mz-logger';
-
 import QuoteForm from './quoteForm';
-
-// const log = mzLogger('NewQuote');
 
 const StyledCard = styled(Card)`
 	margin-top: 10px;
 `;
 
-class NewQuote extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<StyledCard>
-					<QuoteForm />
-				</StyledCard>
-			</React.Fragment>
-		);
-	}
-}
+const NewQuote = () => (
+	<React.Fragment>
+		<StyledCard>
+			<QuoteForm />
+		</StyledCard>
+	</React.Fragment>
+);
 
 export default NewQuote;
