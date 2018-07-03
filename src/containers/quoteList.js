bb2707cbd9f8795d4591df08b529d15ca8660083
Arief Novianto <arief.novianto8@gmail.com>
2018-07-04 06:16:43 +0700

    fix lint err, add loading, add fetching error handling

--- src/containers/quoteList.js
+++ src/containers/quoteList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'; // must be in scope
-import { Pagination } from 'antd';
+import { Pagination, Icon, message } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import isEmpty from 'is-empty';
 
 import mzLogger from '../helpers/mz-logger';
 
 import Quote from '../components/quote';
+
+import style from '../pages/style';
 
 import {
 	setQuotes,
@@ -16,7 +19,14 @@ import {
 const log = mzLogger('QuoteList');
 
 const StyledPagination = styled(Pagination)`
-	margin-top: 10px
+	margin-top: 10px;
+`;
+
+const Loading = styled.div`
+	margin-top: 10px;
+	font-size: 30px;
+	color: ${style.globalColor.hover};
+	text-align: center;
 `;
 
 class QuoteList extends Component {
@@ -24,6 +34,7 @@ class QuoteList extends Component {
 		super(props);
 
 		this.state = {
+			loading: false,
 			page: {
 				current: 1,
 				size: 5,
@@ -32,35 +43,60 @@ class QuoteList extends Component {
 	}
 
 	componentDidMount() {
-		this.props.getQuotes().then((res) => {
-			this.props.setQuotes(res.quotes);
-		});
+		this.setState({ loading: true });
+
+		this.props.getQuotes()
+			.then((res) => {
+				this.props.setQuotes(res.quotes);
+			})
+			.catch((err) => {
+				if (!isEmpty(err.message)) message.error(err.message); // global err
+			})
+			.finally(() => {
+				this.setState({ loading: false });
+			});
 	}
 
 	changePage(next) {
 		const { page } = this.state;
 
 		page.current = next;
-		log.info(`change current page state to ${next}`);
 		this.setState({ page });
+
+		log.info(`change current page state to ${next}`);
 	}
 
 	render() {
-		const { page } = this.state;
+		const { loading, page } = this.state;
 		const { quote } = this.props;
 
 		return (
 			<React.Fragment>
-				{/* immutable */}
-				{[...quote].reverse().slice((page.current - 1) * page.size, page.current * page.size).map((item, index) => (
-					<Quote key={index} id={item.id} content={item.content} inventor={item.inventor} />
-				))}
-				<StyledPagination
-					current={page.current}
-					pageSize={page.size}
-					total={quote.length}
-					onChange={this.changePage.bind(this)}
-				/>
+				{loading ? (
+					<React.Fragment>
+						<Loading>
+							<Icon type="loading" />
+						</Loading>
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						{/* immutable */}
+						{[...quote].reverse().slice((page.current - 1) * page.size, page.current * page.size).map((item, index) => (
+							<Quote
+								key={index}
+								id={item.id}
+								content={item.content}
+								inventor={item.inventor}
+							/>
+						))}
+						<StyledPagination
+							current={page.current}
+							pageSize={page.size}
+							total={quote.length}
+							onChange={this.changePage.bind(this)}
+						/>
+					</React.Fragment>
+				)}
 			</React.Fragment>
 		);
 	}
